8a6628e87f682a5b62326ce2984de5dd73e22166
xodndlee <dnd5436@gmail.com>
2023-10-11 11:03:23 +0900

    test

--- src/test/java/com/example/tourdestination/TourDestinationApplicationTests.java
+++ src/test/java/com/example/tourdestination/TourDestinationApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.tourdestination;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class TourDestinationApplicationTests {
 
 	@Test
